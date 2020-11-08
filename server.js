d90e90c9d9882cd7bb911bf7d99d9e2fb150a09e
unknown <sallyannegaller@gmail.com>
2020-11-08 12:17:40 -0800

    Neater

--- server.js
+++ server.js
@@ -17,17 +17,14 @@ app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
   console.log("validate bearer token middleware");
-
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
   }
-
   next();
 });
 
 app.get("/movie", function handleGetMovie(req, res) {
   let response = MOVIEDEX;
-
   //filter by genre
   if (req.query.genre) {
     response = response.filter((movies) =>
@@ -40,14 +37,12 @@ app.get("/movie", function handleGetMovie(req, res) {
       movies.country.toLowerCase().includes(req.query.country.toLowerCase())
     );
   }
-
   //filter by average vote
   if (req.query.avg_vote) {
     response = response.filter(
       (movies) => Number(movies.avg_vote) >= Number(req.query.avg_vote)
     );
   }
-
   res.json(response);
 });
 
