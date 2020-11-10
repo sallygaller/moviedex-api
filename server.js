7414b37060fb3785357c14c0f642d3a186bf3333
unknown <sallyannegaller@gmail.com>
2020-11-09 18:24:31 -0800

    deploying to production exercise

--- server.js
+++ server.js
@@ -5,18 +5,16 @@ const cors = require("cors");
 const helmet = require("helmet");
 const MOVIEDEX = require("./moviedex.json");
 
-console.log(process.env.API_TOKEN);
-
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
-  console.log("validate bearer token middleware");
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
   }
@@ -46,8 +44,16 @@ app.get("/movie", function handleGetMovie(req, res) {
   res.json(response);
 });
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT);
